c40fca55e1e4e265c965a4d8ee6790f82b099107
prabirshrestha <mail@prabir.me>
2011-11-12 22:51:19 +0545

    + IFacebookUser

---
+++ src/NancyFacebookSample/Models/IFacebookUser.cs
@@ -0,0 +1,26 @@
+﻿namespace NancyFacebookSample.Models
+{
+    public interface IFacebookUser
+    {
+        // append 'Facebook' in order to avoid clahes with property names.
+        // for example there might already be a property named 'Id' in the class that inherits from IUserIdentity.
+
+        /// <summary>
+        /// Gets the Facebook user id.
+        /// </summary>
+        long FacebookId { get; set; }
+
+        /// <summary>
+        /// Gets the Facebook Access Token.
+        /// </summary>
+        string FacebookAccessToken { get; set; }
+
+        /// <summary>
+        /// Gets the Facebook user name.
+        /// </summary>
+        /// <remarks>
+        /// name is actually not useful for authentication nor is it required, but helpful for debuging purposes.
+        /// </remarks>
+        string FacebookName { get; set; }
+    }
+}
