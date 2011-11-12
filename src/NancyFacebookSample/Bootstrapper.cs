799ac8f961006252da7773144359a5421b4e7605
prabirshrestha <mail@prabir.me>
2011-11-12 23:31:35 +0545

    + Bootstrapper register for FacebookClient, IFacebookApplication and FacebookOAuthClient

--- src/NancyFacebookSample/Bootstrapper.cs
+++ src/NancyFacebookSample/Bootstrapper.cs
@@ -1,8 +1,11 @@
 ﻿
 namespace NancyFacebookSample
 {
+    using System;
     using Nancy;
     using Nancy.Authentication.Forms;
+    using Nancy.Extensions;
+    using NancyFacebookSample.Models;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
@@ -24,6 +27,62 @@ namespace NancyFacebookSample
 
             container.Register<Repositories.IAppUserMapper, Repositories.InMemoryAppUserMapper>().AsSingleton();
             container.Register<IUserMapper>(container.Resolve<Repositories.IAppUserMapper>());
+        }
+
+        protected override void ConfigureRequestContainer(TinyIoC.TinyIoCContainer container, NancyContext context)
+        {
+            base.ConfigureRequestContainer(container, context);
+
+            RegisterFacebookClientPerRequest(container, context);
+            RegisterIFacebookApplicationPerRequest(container, context);
+            RegisterFacebookOAuthClientPerRequest(container, context);
+        }
+
+        private void RegisterFacebookClientPerRequest(TinyIoC.TinyIoCContainer container, NancyContext context)
+        {
+            var facebookClient = new Facebook.FacebookClient();
+
+            var fbUser = context.CurrentUser as IFacebookUser;
+            if (fbUser != null)
+                facebookClient.AccessToken = fbUser.FacebookAccessToken;
+
+            if (context.Request != null && context.Request.Url != null)
+                facebookClient.IsSecureConnection = context.Request.Url.Scheme == "https";
+
+            container.Register(facebookClient);
+        }
+
+        private void RegisterIFacebookApplicationPerRequest(TinyIoC.TinyIoCContainer container, NancyContext context)
+        {
+            Facebook.IFacebookApplication facebookApplication = null;
+            if (context != null && context.Request != null && context.Request.Url != null)
+            {
+                var url = context.Request.Url;
+                if (url.HostName == "localhost")
+                {
+                    facebookApplication = new Facebook.DefaultFacebookApplication
+                                              {
+                                                  AppId = ",
+                                                  AppSecret = "
+                                              };
+                }
+                else
+                {
+                    //facebookApplication = new Facebook.DefaultFacebookApplication { AppId = "", AppSecret = "" };
+                }
+            }
+            container.Register(facebookApplication);
+        }
+
+        private void RegisterFacebookOAuthClientPerRequest(TinyIoC.TinyIoCContainer container, NancyContext context)
+        {
+            var facebookOAuthClient =
+                new Facebook.FacebookOAuthClient(container.Resolve<Facebook.IFacebookApplication>())
+                    {
+                        RedirectUri = new Uri("http://localhost:45254" + context.ToFullPath("~/facebook/login/callback"))
+                    };
+
+            container.Register(facebookOAuthClient);
         }
     }
 }
