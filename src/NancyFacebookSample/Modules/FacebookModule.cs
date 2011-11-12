97e3d04c941693623d265a8eabd964d7e6c052dc
prabirshrestha <mail@prabir.me>
2011-11-12 23:34:12 +0545

    + empty FacebookModule

---
+++ src/NancyFacebookSample/Modules/FacebookModule.cs
@@ -0,0 +1,17 @@
+﻿
+namespace NancyFacebookSample.Modules
+{
+    using Nancy;
+    using Nancy.Security;
+
+    public class FacebookModule : NancyModule
+    {
+        public FacebookModule()
+            : base("/facebook")
+        {
+            this.RequiresAuthentication();
+
+            Get["/"] = _ => "fb";
+        }
+    }
+}
