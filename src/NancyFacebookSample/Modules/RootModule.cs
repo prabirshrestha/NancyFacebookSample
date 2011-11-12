1303d3f87afa28f568942013eecddf331136bf4e
prabirshrestha <mail@prabir.me>
2011-11-12 22:47:26 +0545

    + RootModule

---
+++ src/NancyFacebookSample/Modules/RootModule.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NancyFacebookSample.Modules
+{
+    using Nancy;
+
+    public class RootModule : NancyModule
+    {
+        public RootModule()
+        {
+            Get["/"] = parameters => View["index"];
+        }
+    }
+}
