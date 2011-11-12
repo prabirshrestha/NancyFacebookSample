4e32063f4be3d44b968d73c2c91096a5693ed4f1
prabirshrestha <mail@prabir.me>
2011-11-12 22:53:15 +0545

    + User model

---
+++ src/NancyFacebookSample/Models/User.cs
@@ -0,0 +1,19 @@
+﻿
+namespace NancyFacebookSample.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using Nancy.Security;
+
+    public class User : IUserIdentity, IFacebookUser
+    {
+        public Guid Identifier { get; set; }
+
+        public string UserName { get; set; }
+        public IEnumerable<string> Claims { get; set; }
+
+        public long FacebookId { get; set; }
+        public string FacebookAccessToken { get; set; }
+        public string FacebookName { get; set; }
+    }
+}
