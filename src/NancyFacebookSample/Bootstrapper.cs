e55913d4574c15e533352caf94dbfe65ee843509
prabirshrestha <mail@prabir.me>
2011-11-12 23:12:37 +0545

    + Bootstrapper with forms auth enabled

---
+++ src/NancyFacebookSample/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿
+namespace NancyFacebookSample
+{
+    using Nancy;
+    using Nancy.Authentication.Forms;
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoC.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            var formsAuthConfig = new FormsAuthenticationConfiguration
+                                      {
+                                          RedirectUrl = "~/facebook/login",
+                                          UserMapper = container.Resolve<IUserMapper>()
+                                      };
+            FormsAuthentication.Enable(pipelines, formsAuthConfig);
+        }
+    }
+}
