9d791592737fa4cd2c759271ae79c95614ef1389
prabirshrestha <mail@prabir.me>
2011-11-12 23:07:00 +0545

    + IAppUserMapper

---
+++ src/NancyFacebookSample/Repositories/IAppUserMapper.cs
@@ -0,0 +1,11 @@
+﻿namespace NancyFacebookSample.Repositories
+{
+    using Models;
+    using Nancy.Authentication.Forms;
+
+    public interface IAppUserMapper : IUserMapper
+    {
+        void AddOrUpdate(User user);
+        void Remove(User user);
+    }
+}
