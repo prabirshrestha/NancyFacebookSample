b8a3df722dd401602717f13a6c8de7c0f84ae659
prabirshrestha <mail@prabir.me>
2011-11-13 20:31:24 +0545

    + FacebookExtensions for parsing signed_request and FacebookRegistration plugin sample

---
+++ src/NancyFacebookSample/FacebookExtensions.cs
@@ -0,0 +1,145 @@
+﻿
+namespace NancyFacebookSample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Nancy;
+
+    public static class FacebookExtensions
+    {
+        private const string InvalidSignedRequest = "invalid signed_request";
+
+        public static object ParseFacebookSignedRequest(string appSecret, string signedRequestValue)
+        {
+            return TryParseFacebookSignedRequestInternal(appSecret, signedRequestValue, true);
+        }
+
+        public static bool TryParseFacebookSignedRequest(string appSecret, string signedRequestValue, out object signedRequest)
+        {
+            signedRequest = TryParseFacebookSignedRequestInternal(appSecret, signedRequestValue, false);
+            return signedRequest != null;
+        }
+
+        public static object ParseFacebookSignedRequest(this Request request, string appId, string appSecret)
+        {
+            return TryParseFacebookSignedRequestInternal(request, appId, appSecret, true);
+        }
+
+        public static bool TryParseFacebookSignedRequest(this Request request, string appId, string appSecret, out object signedRequest)
+        {
+            signedRequest = TryParseFacebookSignedRequestInternal(request, appId, appSecret, false);
+            return signedRequest != null;
+        }
+
+        private static object TryParseFacebookSignedRequestInternal(string appSecret, string signedRequestValue, bool throws)
+        {
+            if (string.IsNullOrWhiteSpace(appSecret))
+                throw new ArgumentNullException("appSecret");
+            if (string.IsNullOrWhiteSpace(signedRequestValue))
+                throw new ArgumentNullException("signedRequestValue");
+
+            try
+            {
+                string[] split = signedRequestValue.Split('.');
+                if (split.Length != 2)
+                {
+                    // need to have exactly 2 parts
+                    throw new InvalidOperationException(InvalidSignedRequest);
+                }
+
+                string encodedignature = split[0];
+                string encodedEnvelope = split[1];
+
+                if (string.IsNullOrWhiteSpace(encodedignature) || string.IsNullOrWhiteSpace(encodedEnvelope))
+                    throw new InvalidOperationException(InvalidSignedRequest);
+
+                var envelope = (IDictionary<string, object>)Facebook.JsonSerializer.Current.DeserializeObject(Encoding.UTF8.GetString(Base64UrlDecode(encodedEnvelope)));
+                var algorithm = (string)envelope["algorithm"];
+                if (!algorithm.Equals("HMAC-SHA256"))
+                    throw new InvalidOperationException("Unknown algorithm. Expected HMAC-SHA256");
+
+                byte[] key = Encoding.UTF8.GetBytes(appSecret);
+                byte[] digest = ComputeHmacSha256Hash(Encoding.UTF8.GetBytes(encodedEnvelope), key);
+
+                if (!digest.SequenceEqual(Base64UrlDecode(encodedignature)))
+                    throw new InvalidOperationException(InvalidSignedRequest);
+                return envelope;
+            }
+            catch
+            {
+                if (throws)
+                    throw;
+                return null;
+            }
+        }
+
+        private static object TryParseFacebookSignedRequestInternal(Request request, string appId, string appSecret, bool throws)
+        {
+            if (request == null)
+                throw new ArgumentNullException("request");
+            if (string.IsNullOrWhiteSpace(appSecret))
+                throw new ArgumentNullException("appSecret");
+
+            object signedRequest = null;
+            if (request.Form.signed_request.HasValue && !string.IsNullOrWhiteSpace(request.Form.signed_request))
+            {
+                signedRequest = TryParseFacebookSignedRequestInternal(appSecret, request.Form.signed_request, throws);
+            }
+
+            if (signedRequest == null)
+            {
+                if(string.IsNullOrWhiteSpace(appId))
+                    throw new ArgumentNullException("appId");
+                string signedRequestCookieValue;
+                if (request.Cookies.TryGetValue("fbsr_" + appId, out signedRequestCookieValue))
+                {
+                    if (!string.IsNullOrWhiteSpace(signedRequestCookieValue))
+                        signedRequest = TryParseFacebookSignedRequestInternal(appSecret, signedRequestCookieValue, throws);
+                }
+            }
+
+            return signedRequest;
+        }
+
+        /// <summary>
+        /// Base64 Url decode.
+        /// </summary>
+        /// <param name="base64UrlSafeString">
+        /// The base 64 url safe string.
+        /// </param>
+        /// <returns>
+        /// The base 64 url decoded string.
+        /// </returns>
+        private static byte[] Base64UrlDecode(string base64UrlSafeString)
+        {
+            if (string.IsNullOrEmpty(base64UrlSafeString))
+                throw new ArgumentNullException("base64UrlSafeString");
+
+            base64UrlSafeString = base64UrlSafeString.PadRight(base64UrlSafeString.Length + (4 - base64UrlSafeString.Length % 4) % 4, '=');
+            base64UrlSafeString = base64UrlSafeString.Replace('-', '+').Replace('_', '/');
+            return Convert.FromBase64String(base64UrlSafeString);
+        }
+
+        /// <summary>
+        /// Computes the Hmac Sha 256 Hash.
+        /// </summary>
+        /// <param name="data">
+        /// The data to hash.
+        /// </param>
+        /// <param name="key">
+        /// The hash key.
+        /// </param>
+        /// <returns>
+        /// The Hmac Sha 256 hash.
+        /// </returns>
+        private static byte[] ComputeHmacSha256Hash(byte[] data, byte[] key)
+        {
+            using (var crypto = new System.Security.Cryptography.HMACSHA256(key))
+            {
+                return crypto.ComputeHash(data);
+            }
+        }
+    }
+}
---
+++ src/NancyFacebookSample/Modules/FacebookRegistrationModule.cs
@@ -0,0 +1,82 @@
+﻿
+namespace NancyFacebookSample.Modules
+{
+    using System;
+    using System.Dynamic;
+    using Facebook;
+    using Nancy;
+    using Nancy.Authentication.Forms;
+    using Nancy.Extensions;
+    using Nancy.Helpers;
+    using NancyFacebookSample.Models;
+    using Repositories;
+
+    public class FacebookRegistrationModule : NancyModule
+    {
+        public FacebookRegistrationModule(IFacebookApplication facebookApplication, IAppUserMapper userMapper)
+        {
+            Get["/register"] =
+                _ =>
+                {
+                    // note: for more options to https://developers.facebook.com/docs/plugins/registration/
+
+                    var fields = new object[]
+                                         {
+                                             new {name = "name"},
+                                             new {name = "email"},
+                                             new {name = "location"},
+                                             new {name = "gender"},
+                                             new {name = "birthday"},
+                                             new {name = "password", view = "not_prefilled"},
+                                             new
+                                                 {
+                                                     name = "like",
+                                                     description = "Do you like this plugin?",
+                                                     type = "checkbox",
+                                                     @default = "checked"
+                                                 },
+                                             new
+                                                 {
+                                                     name = "phone",
+                                                     description = "Phone Number",
+                                                     type = "text"
+                                                 },
+                                             new {name = "captcha"}
+                                         };
+
+                    dynamic model = new ExpandoObject();
+                    model.FacebookRegistrationUrl = string.Format(
+                        "http://www.facebook.com/plugins/registration.php?client_id={0}&redirect_uri={1}&fields={2}&fb_only=true",
+                        facebookApplication.AppId,
+                        HttpUtility.UrlEncode("http://localhost:45254" + Context.ToFullPath("~/register/facebookcallback")),
+                        HttpUtility.UrlEncode(JsonSerializer.Current.SerializeObject(fields)));
+
+                    return View["register", model];
+                };
+
+            Post["/register/facebookcallback"] =
+                _ =>
+                {
+                    dynamic signedRequest = Request.ParseFacebookSignedRequest(facebookApplication.AppId, facebookApplication.AppSecret);
+                    DateTime expiresOn = signedRequest.expires == 0 ? DateTime.MaxValue : DateTime.UtcNow.AddSeconds(Convert.ToDouble(signedRequest.expires));
+                    DateTime issuedAt = DateTimeConvertor.FromUnixTime(signedRequest.issued_at);
+                    var accessToken = signedRequest.oauth_token;
+
+                    var name = signedRequest.registration.name;
+                    var userId = Convert.ToInt64(signedRequest.user_id);
+
+                    var user = new User
+                                   {
+                                       FacebookAccessToken = accessToken,
+                                       FacebookId = userId,
+                                       FacebookName = name,
+                                       UserName = name
+                                   };
+
+                    userMapper.AddOrUpdate(user);
+
+                    return this.LoginAndRedirect(user.Identifier, expiresOn, "~/facebook");
+                };
+        }
+    }
+}
